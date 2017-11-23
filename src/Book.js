6f9fc997746eb94329cec8bea1ddb438dc3b5998
Diogo Silvério <dblsilverio@gmail.com>
2017-11-22 22:42:12 -0200

    refactor: Book component improvement
    Checking if author is present, otherwise assuming publisher as author name to display.
    
    Selecting flags(Reading, Want to read...) correctly.

--- src/Book.js
+++ src/Book.js
@@ -3,12 +3,26 @@ import React, { Component } from 'react';
 export default class Book extends Component {
 
     render() {
+
+        let authors = this.props.info.authors;
+        let actualShelf = this.props.info.shelf;
+
+        if(authors){
+            authors = authors.join(', ');
+        } else {
+            authors = this.props.info.publisher;
+        }
+
+        if(!actualShelf){
+            actualShelf = "none";
+        }
+
         return (
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.info.imageLinks.smallThumbnail}")` }}></div>
                     <div className="book-shelf-changer">
-                        <select>
+                        <select value={actualShelf}>
                             <option value="none" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
@@ -18,7 +32,7 @@ export default class Book extends Component {
                     </div>
                 </div>
                 <div className="book-title">{this.props.info.title}</div>
-                <div className="book-authors">{this.props.info.authors.join(', ')}</div>
+                <div className="book-authors">{authors}</div>
             </div>
         );
     }
